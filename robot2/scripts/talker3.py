3b919b1aaa14016c701b6297bc16c980044b21f3
averak <abe12@mccc.jp>
2021-06-09 03:50:26 +0900

    [feat] create talter3

---
+++ robot2/scripts/talker3.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+
+
+def callback(data):
+    rospy.loginfo(data.data)
+
+
+def talker():
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    sub = rospy.Subscriber('chatter', String, callback)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10)  # 10hz
+    while not rospy.is_shutdown():
+        message = input("[keyinput]: ")
+        pub.publish(message)
+        rate.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
